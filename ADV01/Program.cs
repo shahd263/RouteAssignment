b3edef7ccb3d543fc5c7a9de9caf23ed14957ea1
Shahd <shahd.mostafa12@gmail.com>
2025-08-08 03:24:58 +0300

    'Assignment01'

---
+++ ADV01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ADV01
+{
+
+
+    internal class Program
+    {
+        #region Q1
+        //The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm 
+        //And implement the code of this optimized bubble sort algorithm
+
+        public static void BubbleSort<T>(T[] array) where T : IComparable<T>
+        {
+            if (array is not null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    bool check = false;
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                    {
+                        if (array[j].CompareTo(array[j + 1]) == 1)
+                        {
+                            T temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            check = true;
+                        }
+
+
+                    }
+                    if (!check) return;
+                }
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            #region Q1
+            //int[] arr = { 1, 3, 4, 7, 2, 8, 5, 6 };
+            //BubbleSort<int>(arr);
+            //foreach (int i in arr)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+
+            #region Q2
+            Range<int> r = new Range<int>(10, 1);
+            if(r.IsInRange(11))
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+
+            Console.WriteLine($"Length = {r.Length()}");
+
+
+            #endregion
+
+
+
+        }
+    }
+}
---
+++ ADV01/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV01
+{
+    internal class Range<T> where T : IComparable<T>  , INumber<T>
+    {
+        public T  Max{ get; set; }
+        public T Min { get; set; }
+
+        public Range(T max ,T min) {
+            Max = max;  
+            Min = min;
+        }
+
+        public bool IsInRange(T value) {
+            return Max.CompareTo(value) >= 0 && Min.CompareTo(value) <= 0;
+                
+        }
+
+        public T Length()
+        {
+            return Max - Min;
+        }
+
+
+
+    }
+}
